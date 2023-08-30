b1c2cea42ff6d1155906a0058f3d76c831dbe837
Ade Coker <88948587+adecoker@users.noreply.github.com>
2023-08-30 01:32:46 -0400

    Update 04-user_defined_vars.sh

--- sc/04-user_defined_vars.sh
+++ sc/04-user_defined_vars.sh
@@ -1,13 +1,13 @@
 #!/bin/bash      
 #title           :
 #description     :
-#author		     :myLANDMARK.tech S Legah
-#date            :08112012
-#version         :1.0    
-#Tel		     :437 215 2483
+author		     :AJJJC Groups Ade Coker
+date            :08292023
+version         :1.0    
+Tel		     :562 253 9414
 
-name="Simon Legah"
-id=08112012
+name="Ade"
+id=08292023
 
 echo "The name varibale value is: "$name
 echo "The id variable value is: "$id
