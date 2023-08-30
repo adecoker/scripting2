27b1dd60a236578adda273cbb30beda64de16a95
Ade Coker <88948587+adecoker@users.noreply.github.com>
2023-08-30 01:39:51 -0400

    Update 08-arithmetic_operations.sh

--- 08-arithmetic_operations.sh
+++ 08-arithmetic_operations.sh
@@ -1,6 +1,10 @@
 #!/bin/bash      
 #title           : Arithmetic operations
 
+author		     :AJJJC Groups Ade Coker
+date            :08292023
+version         :1.0    
+Tel		     : 562 253 9414
 expr 3 + 2
 expr 3 - 2
 expr 3 \* 2
