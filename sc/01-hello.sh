25298329e964108411c73f7990fad9b4ec6740ba
Ade Coker <88948587+adecoker@users.noreply.github.com>
2023-08-30 01:31:33 -0400

    Update 01-hello.sh

--- sc/01-hello.sh
+++ sc/01-hello.sh
@@ -1,6 +1,8 @@
 #!/bin/bash      
-
-echo "Hello Guys"
+author		     :AJJJC Groups Ade Coker
+date            :08292023
+version         :1.0    
+Tel		     :562 253 9414echo "Hello Guys"
 echo "Welcome to ShellScript"
 echo "This is the first shell script example !!"
 echo "Today date is"
